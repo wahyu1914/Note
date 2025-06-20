c2230361850736c2227aaa1efcafb1f71a37d34a
wahyu1914 <wahyu582147@gmail.com>
2025-06-20 21:09:18 +0700

    Updated

---
+++ lib/core/validator.dart
@@ -0,0 +1,34 @@
+class Validator {
+  Validator._();
+
+  static String? nameValidator(String? name) {
+    name = name?.trim() ?? '';
+
+    return name.isEmpty ? 'No name provided!' : null;
+  }
+
+  static const String _emailPattern =
+      r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'"
+      r'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-'
+      r'\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*'
+      r'[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4]'
+      r'[0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9]'
+      r'[0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\'
+      r'x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])';
+
+  static String? emailValidator(String? email) {
+    email = email?.trim() ?? '';
+
+    return email.isEmpty
+        ? 'No email provided!'
+        : !RegExp(_emailPattern).hasMatch(email)
+        ? 'Password is not in a valid format!'
+        : null;
+  }
+
+  static String? passwordValidator(String? password) {
+    password = password?.trim() ?? '';
+
+    return password.isEmpty ? 'No password provided!' : null;
+  }
+}
--- lib/pages/registration_page.dart
+++ lib/pages/registration_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:project_note/change_notifiers/registration_controller.dart';
+import 'package:project_note/core/validator.dart';
 import 'package:project_note/widgets/note_button.dart';
 import 'package:project_note/widgets/note_form_field.dart';
 import 'package:project_note/widgets/note_icon_button_outlined.dart';
@@ -55,146 +56,157 @@ class _RegistrationPageState extends State<RegistrationPage> {
             child: SingleChildScrollView(
               child: Selector<RegistrationController, bool>(
                 selector: (_, controller) => controller.isRegisterMode,
-                builder: (_, isRegisterMode, __) => Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Text(
-                      isRegisterMode ? 'Register' : 'Sign In',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 48,
-                        fontFamily: 'Fredoka',
-                        fontWeight: FontWeight.w600,
-                        color: primary,
+                builder: (_, isRegisterMode, __) => Form(
+                  key: formKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Text(
+                        isRegisterMode ? 'Register' : 'Sign In',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 48,
+                          fontFamily: 'Fredoka',
+                          fontWeight: FontWeight.w600,
+                          color: primary,
+                        ),
                       ),
-                    ),
-                    SizedBox(height: 16),
-                    Text(
-                      'In order to sync your notes to cloud, you have to register/sign in to the app',
-                      textAlign: TextAlign.center,
-                    ),
-                    SizedBox(height: 48),
-                    if (isRegisterMode) ...[
+                      SizedBox(height: 16),
+                      Text(
+                        'In order to sync your notes to cloud, you have to register/sign in to the app',
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(height: 48),
+                      if (isRegisterMode) ...[
+                        NoteFormField(
+                          controller: nameController,
+                          labelText: 'Full name',
+                          fillColor: white,
+                          filled: true,
+                          textCapitalization: TextCapitalization.sentences,
+                          textInputAction: TextInputAction.next,
+                          validator: Validator.nameValidator,
+                        ),
+                        SizedBox(height: 8),
+                      ],
                       NoteFormField(
-                        controller: nameController,
-                        labelText: 'Full name',
+                        controller: emailController,
+                        labelText: 'Email address',
                         fillColor: white,
                         filled: true,
-                        textCapitalization: TextCapitalization.sentences,
+                        keyboardType: TextInputType.emailAddress,
                         textInputAction: TextInputAction.next,
+                        validator: Validator.emailValidator,
                       ),
                       SizedBox(height: 8),
-                    ],
-                    NoteFormField(
-                      controller: emailController,
-                      labelText: 'Email address',
-                      fillColor: white,
-                      filled: true,
-                      keyboardType: TextInputType.emailAddress,
-                      textInputAction: TextInputAction.next
-                    ),
-                    SizedBox(height: 8),
-                    Selector<RegistrationController, bool>(
-                      selector: (_, controller) => controller.isPasswordHidden,
+                      Selector<RegistrationController, bool>(
+                        selector: (_, controller) =>
+                            controller.isPasswordHidden,
 
-                      builder: (_, isPasswordHidden, __) => NoteFormField(
-                        controller: passwordController,
-                        labelText: 'Password',
-                        fillColor: white,
-                        filled: true,
-                        obscureText: isPasswordHidden,
-                        suffixIcon: GestureDetector(
-                          onTap: () {
-                            registrationController.isPasswordHidden =
-                                !isPasswordHidden;
-                          },
-                          child: Icon(
-                            isPasswordHidden
-                                ? FontAwesomeIcons.eye
-                                : FontAwesomeIcons.eyeSlash,
-                          ),
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 12),
-                    if (!isRegisterMode) ...[
-                      Text(
-                        'Forgot password?',
-                        style: TextStyle(
-                          color: primary,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(height: 24),
-                    ],
-                    SizedBox(
-                      height: 48,
-                      child: NoteButton(
-                        label: isRegisterMode
-                            ? 'Create My Account'
-                            : 'Log me in',
-                        onPressed: () {},
-                      ),
-                    ),
-                    SizedBox(height: 32),
-                    Row(
-                      children: [
-                        Expanded(child: Divider()),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                          child: Text(
-                            isRegisterMode
-                                ? 'Or register with'
-                                : 'Or sign in with',
-                          ),
-                        ),
-                        Expanded(child: Divider()),
-                      ],
-                    ),
-                    SizedBox(height: 32),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: NoteIconButtonOutlined(
-                            icon: FontAwesomeIcons.google,
-                            onPressed: () {},
-                          ),
-                        ),
-                        SizedBox(width: 16),
-                        Expanded(
-                          child: NoteIconButtonOutlined(
-                            icon: FontAwesomeIcons.facebook,
-                            onPressed: () {},
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 32),
-                    Text.rich(
-                      TextSpan(
-                        text: isRegisterMode
-                            ? 'Already have an account? '
-                            : 'Dont have an account',
-                        style: TextStyle(color: gray700),
-                        children: [
-                          TextSpan(
-                            text: isRegisterMode ? 'Sign in' : 'Register',
-                            style: TextStyle(
-                              color: primary,
-                              fontWeight: FontWeight.bold,
+                        builder: (_, isPasswordHidden, __) => NoteFormField(
+                          controller: passwordController,
+                          labelText: 'Password',
+                          fillColor: white,
+                          filled: true,
+                          obscureText: isPasswordHidden,
+                          suffixIcon: GestureDetector(
+                            onTap: () {
+                              registrationController.isPasswordHidden =
+                                  !isPasswordHidden;
+                            },
+                            child: Icon(
+                              isPasswordHidden
+                                  ? FontAwesomeIcons.eye
+                                  : FontAwesomeIcons.eyeSlash,
                             ),
-                            recognizer: TapGestureRecognizer()
-                              ..onTap = () {
-                                registrationController.isRegisterMode =
-                                    !isRegisterMode;
-                              },
+                          ),
+                          validator: Validator.passwordValidator,
+                        ),
+                      ),
+                      SizedBox(height: 12),
+                      if (!isRegisterMode) ...[
+                        Text(
+                          'Forgot password?',
+                          style: TextStyle(
+                            color: primary,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 24),
+                      ],
+                      SizedBox(
+                        height: 48,
+                        child: NoteButton(
+                          label: isRegisterMode
+                              ? 'Create My Account'
+                              : 'Log me in',
+                          onPressed: () {
+                            formKey.currentState?.validate();
+                          },
+                        ),
+                      ),
+                      SizedBox(height: 32),
+                      Row(
+                        children: [
+                          Expanded(child: Divider()),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 16.0,
+                            ),
+                            child: Text(
+                              isRegisterMode
+                                  ? 'Or register with'
+                                  : 'Or sign in with',
+                            ),
+                          ),
+                          Expanded(child: Divider()),
+                        ],
+                      ),
+                      SizedBox(height: 32),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: NoteIconButtonOutlined(
+                              icon: FontAwesomeIcons.google,
+                              onPressed: () {},
+                            ),
+                          ),
+                          SizedBox(width: 16),
+                          Expanded(
+                            child: NoteIconButtonOutlined(
+                              icon: FontAwesomeIcons.facebook,
+                              onPressed: () {},
+                            ),
                           ),
                         ],
                       ),
-                      textAlign: TextAlign.center,
-                    ),
-                  ],
+                      SizedBox(height: 32),
+                      Text.rich(
+                        TextSpan(
+                          text: isRegisterMode
+                              ? 'Already have an account? '
+                              : 'Dont have an account',
+                          style: TextStyle(color: gray700),
+                          children: [
+                            TextSpan(
+                              text: isRegisterMode ? 'Sign in' : 'Register',
+                              style: TextStyle(
+                                color: primary,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () {
+                                  registrationController.isRegisterMode =
+                                      !isRegisterMode;
+                                },
+                            ),
+                          ],
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
